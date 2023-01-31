58b67088383f4bf0132d98f32b50a3e5801dfd64
Vadym Rud <rudvadim@gmail.com>
2023-01-31 18:00:34 +0200

    added base modules

---
+++ starsh/starsh/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-y)8!jfdbm_5+-1hwudkt8%gatz$o)cmcj7%y0_)f$+%_l0zc*0"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
