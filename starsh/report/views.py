f7b29d238c403c2c24eb6018e293edb2bf141319
Vadym Rud <rudvadim@gmail.com>
2023-03-01 12:54:57 +0200

    remove shit code from staff

--- starsh/report/views.py
+++ starsh/report/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.template.loader import render_to_string
-from django_tables2 import SingleTableView, RequestConfig
+from django_tables2 import SingleTableView, RequestConfig, MultiTableMixin
 from django.views import View
+from django.views.generic.base import TemplateView
 from django.http import HttpResponse
 from staff.models import Staff
 from gunwarehouse.models import Invoice as gunwarehouse_invoice
@@ -14,11 +15,33 @@ class StaffListView(SingleTableView):
     template_name = 'report/staff_list.html'
 
 
-class SoldierView(View):
+# class SoldierView(View):
+#
+#     def get(self, request, id):
+#         in_hand = gunwarehouse_invoice.objects.filter(purpose='2', responsible_recipient__id=id)
+#         gunwarehouse_invoices = GunWarehouseInvoiceTable(in_hand, prefix="1-")
+#         RequestConfig(request, paginate=False).configure(gunwarehouse_invoices)
+#         #hhhhhhh`asedrty7895trfr5rtfy6
+#         rendered = render_to_string('report/invoices.html', {'gunwarehouse_invoices': gunwarehouse_invoices})
+#         return HttpResponse(rendered)
 
-    def get(self, request, id):
-        in_hand = gunwarehouse_invoice.objects.filter(purpose='2', responsible_recipient__id=id)
-        gunwarehouse_invoices = GunWarehouseInvoiceTable(in_hand, prefix="1-")
-        RequestConfig(request, paginate=False).configure(gunwarehouse_invoices)
-        #hhhhhhh
-        return HttpResponse({'gunwarehouse_invoices': gunwarehouse_invoices})
+
+class SoldierView(MultiTableMixin, TemplateView):
+    template_name = 'report/invoices.html'
+    tables = None
+
+
+
+
+    table_pagination = {
+        'per_page': 10
+    }
+
+    def get_tables(self):
+        qs = gunwarehouse_invoice.objects.filter(purpose='2', responsible_recipient__id=self.kwargs.get('id'))
+        qs1 = gunwarehouse_invoice.objects.all()
+        self.tables = [
+            GunWarehouseInvoiceTable(qs),
+            GunWarehouseInvoiceTable(qs1)
+        ]
+        return self.tables
--- starsh/starsh/settings/base.py
+++ starsh/starsh/settings/base.py
@@ -91,9 +91,14 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
+
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
 
+                "django.template.context_processors.static",
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.request",
+                "django.template.context_processors.static",
             ],
         },
     },
